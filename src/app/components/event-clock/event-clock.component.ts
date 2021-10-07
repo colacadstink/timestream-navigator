724a8b72c7ced111a21d919275670be372bdd1e5
Ben Harris <benharris5@gmail.com>
2021-10-06 22:05:52 -0400

    Minor bugfix for events ending

--- src/app/components/event-clock/event-clock.component.ts
+++ src/app/components/event-clock/event-clock.component.ts
@@ -104,8 +104,6 @@ export class EventClockComponent implements OnInit {
       this._timeLeft = endTime - new Date(timer.serverTime).getTime();
       this._endDate = new Date(now + this._timeLeft);
 
-      this._minSecString = this.getMinSecString();
-
       const idleLoop = () => {
         this._minSecString = this.getMinSecString();
         if(this.isTimerRunning) {
@@ -116,6 +114,8 @@ export class EventClockComponent implements OnInit {
     } else {
       this._endDate = null;
     }
+
+    this._minSecString = this.getMinSecString();
   }
 
   private getMinSecString() {
